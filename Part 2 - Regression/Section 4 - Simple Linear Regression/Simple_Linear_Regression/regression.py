73b95f67102321eab39d9a1325bdfed77a5cd822
harshitm98 <harshitmah98@gmail.com>
2018-05-03 23:50:32 +0530

    Completed Section 4

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/regression.py
@@ -15,11 +15,11 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -28,3 +28,27 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting simple linear regression to the training set￼
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predecting the test set results
+y_pred = regressor.predict(X_test)
+
+#Visualizing the training set result
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color='green')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualizing the test set result
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color='green')
+plt.title('Salary vs Experience(Training set)')
+plt.xlabel('Years of experience')
+plt.ylabel('Salary')
+plt.show()
