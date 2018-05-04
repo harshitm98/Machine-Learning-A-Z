5121f57d011815fc706e1bf167b8c195e10951c5
harshitm98 <harshitmah98@gmail.com>
2018-05-05 03:31:05 +0530

    Downloaded material for Section 7

--- Part 2 - Regression/Section 8 - Decision Tree Regression/regression_template.py
+++ Part 2 - Regression/Section 8 - Decision Tree Regression/regression_template.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May  5 01:20:43 2018
-
-@author: Harshit Maheshwari
-"""
-
-# Support Vector Regression
+# Regression Template
 
 # Importing the libraries
 import numpy as np
@@ -22,35 +15,33 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-from sklearn.preprocessing import StandardScaler
+"""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
+y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting the Regression Model to the dataset
-from sklearn.svm import SVR
-regressor = SVR(kernel = 'rbf')
-regressor.fit(X,y)
+# Create your regressor here
 
 # Predicting a new result
-y_pred = regressor.predict(sc_X.transform(np.array([6.5])))
-y_pred = sc_y.inverse_transform(y_pred)
+y_pred = regressor.predict(6.5)
 
-# Visualising the SVR results
+# Visualising the Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
+plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the SVR results (for higher resolution and smoother curve)
+# Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
+plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
