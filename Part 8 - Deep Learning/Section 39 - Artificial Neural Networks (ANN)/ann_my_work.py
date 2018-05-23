37dd6801d56d0bfe396ad89d592914755bbb0f1d
harshitm98 <harshitmah98@gmail.com>
2018-05-24 02:44:01 +0530

    Completed Section 39

---
+++ Part 8 - Deep Learning/Section 39 - Artificial Neural Networks (ANN)/ann_my_work.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 24 00:52:51 2018
+
+@author: Harshit Maheshwari
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+                
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X_1 = LabelEncoder()
+X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
+labelencoder_X_2 = LabelEncoder()
+X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+X = onehotencoder.fit_transform(X).toarray()
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Making the ANN
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initializing the ANN
+classifier = Sequential()
+
+# Adding input layer and first hidden layer
+classifier.add(Dense(output_dim = 6,init = 'uniform', activation = 'relu', input_dim = 11))
+
+# Adding the second hidden layer
+classifier.add(Dense(output_dim = 6,init = 'uniform', activation = 'relu')) # relu -> rectifier
+
+# Adding the output layer
+classifier.add(Dense(output_dim = 1,init = 'uniform', activation = 'sigmoid')) # to get proabilty
+# activation = 'softmax' if output_dim >= 1 for eg output_dim = 3 
+
+# Compliting the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+# For more than one output loss = 'ffs google it'
+
+# Fitting the ANN to the training set
+classifier.fit(X_train, y_train, batch_size = 10, nb_epoch = 100) # batch_size = number of observations you want to update the wait
+              
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
